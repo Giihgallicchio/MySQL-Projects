86fe18608e62048e2f05d92cc4a1418b4d818f00
Giovanna Galichio <giovannagallicchio7@gmail.com>
2026-03-29 20:09:35 -0300

    Delete MySQL Projects directory

--- MySQL Projects/Cadastro de pessoas.sql
+++
@@ -1,6 +0,0 @@
-insert into pessoas values
-(DEFAULT ,'Claudio', '2001-7-5', 'M', 90.0, 1.67, 'Japão'),
-(DEFAULT ,'Gustavo', '2000-8-1', 'M', 80, 2, default),
-(DEFAULT ,'Miguel', '1987-11-12', 'M', 40.6, 1.66, 'EUA');
-
-select * from pessoas;
